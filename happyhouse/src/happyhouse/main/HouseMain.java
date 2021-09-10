beeb1c3f55dc731e3b9419a9116a15c6cd3eff3a
이정훈 <person00333@gmail.com>
2021-09-10 08:33:46 +0000

    Update HouseMain.java

--- happyhouse/src/happyhouse/main/HouseMain.java
+++ happyhouse/src/happyhouse/main/HouseMain.java
@@ -2,7 +2,15 @@ package happyhouse.main;
 import java.io.*;
 import java.util.List;
 
+import happyhouse.DAO.FavoriteDao;
+import happyhouse.DAO.FavoriteDaoImpl;
+import happyhouse.DAO.HouseDaoImpl;
+import happyhouse.DAO.House_dealDaoImpl;
 import happyhouse.DAO.UserDaoImpl;
+
+import happyhouse.DTO.FavoriteDto;
+import happyhouse.DTO.HouseDto;
+import happyhouse.DTO.House_dealDto;
 import happyhouse.DTO.UserDto;
 
 public class HouseMain {
@@ -38,15 +46,16 @@ public class HouseMain {
 			try {
 				int num = Integer.parseInt(in.readLine());
 				switch(num) {
-//				case 1 : searchAll();break;
-//				case 2 : search_deal();break;
+				case 1 : searchAll();break;
+				case 2 : search_deal();break;
 				case 3 : login();break;
 				case 4 : insert_User();break;
 				case 5 : update_User();break;
 				case 6 : delete_User();break;
 				case 7 : searchAll_User();break;
-				case 8 : break;
-				case 9 : break;
+				case 8 : register_favorite();break;
+				case 9 : deleteFavorite();break;
+				case 10: search_favorite();break;
 				default : isContinue = false;
 				}
 			} catch (IOException e) {
@@ -55,54 +64,121 @@ public class HouseMain {
 		}
 		System.out.println("@@@@@@@@@@@ 프로그램 종료 @@@@@@@@@@@");
 	}
+	private void deleteFavorite() {
+//		System.out.println("deleteProduct");
+		try {
+			System.out.println("--------------------- 상품 삭제 ---------------------");
+			int user_no = user.getUserno();
+			System.out.print("삭제할 동코드 : ");
+			int dongcode = Integer.parseInt(in.readLine());
+			
+			FavoriteDaoImpl.getProductDao().deleteFavorite(user_no, dongcode);
+			System.out.println("-------------------------------------------------");
+			System.out.println("삭제 성공!!!!!");
+			System.out.println("-------------------------------------------------");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
-//	private void searchAll() throws NumberFormatException, IOException {
-////		System.out.println("searchAll");
-//		int guguncode = 11110;
-//		String dong = "사직동";
-//		System.out.print("구군 코드 입력 : ");
-//		guguncode = Integer.parseInt(in.readLine());
-//		System.out.print("동 입력 : ");
-//		dong = in.readLine();
-//		List<HouseDto> list = HouseDaoImpl.getProductDao().searchAll(guguncode,dong);
-//		showList(list);
-//	}
-//	
-//	private void search_deal() throws NumberFormatException, IOException {
-////		System.out.println("searchAll");
-//		int guguncode = 11110;
-//		String dong = "사직동";
-//		String AptName = "광화문풍림스페이스본(101동~105동)";
-//		System.out.print("구군 코드 입력 : ");
-//		guguncode = Integer.parseInt(in.readLine());
-//		System.out.print("동 입력 : ");
-//		dong = in.readLine();
-//		List<House_dealDto> list = House_dealDaoImpl.getProductDao().searchAll(guguncode,dong,AptName);
-//		show_dealList(list);
-//	}
-//
-//	
-//	private void show_dealList(List<House_dealDto> list) {
-//		System.out.println("===================================== 상품 목록 =====================================");
-//		System.out.println("Name\t\tdealAmount\tdealDate\tarea\t\ttype");
-//		
-//		System.out.println("-------------------------------------------------------------------------------------");
-//		for(House_dealDto house : list) {
-//			System.out.println(house);
-//		}
-//		System.out.println("-------------------------------------------------------------------------------------");
-//	}
-//
-//	private void showList(List<HouseDto> list) {
-//		System.out.println("===================================== 상품 목록 =====================================");
-//		System.out.println("동\tName\t구군코드\t건설년\t위도\t경도");
-//		System.out.println("-------------------------------------------------------------------------------------");
-//		for(HouseDto house : list) {
-//			System.out.println(house);
-//		}
-//		System.out.println("-------------------------------------------------------------------------------------");
-//	}
-//	
+	
+	private void register_favorite() {
+//		System.out.println("register");
+//		유효성 검사는 생략할게요!!!
+		try {
+			System.out.println("--------------------- 등록 ---------------------");
+			int user_no = user.getUserno();
+			System.out.print("동 코드 : ");
+			int dongcode = Integer.parseInt(in.readLine());
+			System.out.print("구 : ");
+			String gu = in.readLine();
+			System.out.print("동 : ");
+			String dong = in.readLine();
+			
+			FavoriteDto favoriteDto = new FavoriteDto();
+			favoriteDto.setUser_no(user_no);
+			favoriteDto.setDongcode(dongcode);
+			favoriteDto.setGu(gu);
+			favoriteDto.setDong(dong);
+			
+			FavoriteDao productDao = FavoriteDaoImpl.getProductDao();
+			productDao.register(favoriteDto);
+			System.out.println("-------------------------------------------------");
+			System.out.println("등록 성공!!!!!");
+			System.out.println("-------------------------------------------------");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void search_favorite() throws NumberFormatException, IOException {
+		System.out.println("searchAll");
+		int user_no = user.getUserno();
+
+		List<FavoriteDto> list = FavoriteDaoImpl.getProductDao().searchAll(user_no);
+		show_favoriteList(list);
+	}
+
+	
+	private void show_favoriteList(List<FavoriteDto> list) {
+		System.out.println("===================================== 관심지역 목록 =====================================");
+		System.out.println("user_no\tdongcode\tgu\tdong");
+		
+		System.out.println("-------------------------------------------------------------------------------------");
+		for(FavoriteDto house : list) {
+			System.out.println(house);
+		}
+		System.out.println("-------------------------------------------------------------------------------------");
+	}
+	
+	private void search_deal() throws NumberFormatException, IOException {
+//		System.out.println("searchAll");
+		int guguncode = 11110;
+		String dong = "사직동";
+		String AptName = "광화문풍림스페이스본(101동~105동)";
+		System.out.print("구군 코드 입력 : ");
+		guguncode = Integer.parseInt(in.readLine());
+		System.out.print("동 입력 : ");
+		dong = in.readLine();
+		List<House_dealDto> list = House_dealDaoImpl.getProductDao().searchAll(guguncode,dong,AptName);
+		show_dealList(list);
+	}
+
+	
+	private void show_dealList(List<House_dealDto> list) {
+		System.out.println("===================================== 실거래가 목록 =====================================");
+		System.out.println("Name\t\tdealAmount\tdealDate\tarea\t\ttype");
+		
+		System.out.println("-------------------------------------------------------------------------------------");
+		for(House_dealDto house : list) {
+			System.out.println(house);
+		}
+		System.out.println("-------------------------------------------------------------------------------------");
+	}
+	
+	private void searchAll() throws NumberFormatException, IOException {
+//		System.out.println("searchAll");
+		int guguncode = 11110;
+		String dong = "사직동";
+		System.out.print("구군 코드 입력 : ");
+		guguncode = Integer.parseInt(in.readLine());
+		System.out.print("동 입력 : ");
+		dong = in.readLine();
+		List<HouseDto> list = HouseDaoImpl.getProductDao().searchAll(guguncode,dong);
+		showList(list);
+	}
+	
+	
+
+	private void showList(List<HouseDto> list) {
+		System.out.println("===================================== 목록 =====================================");
+		System.out.println("동\tName\t구군코드\t건설년\t위도\t경도");
+		System.out.println("-------------------------------------------------------------------------------------");
+		for(HouseDto house : list) {
+			System.out.println(house);
+		}
+		System.out.println("-------------------------------------------------------------------------------------");
+	}
 	private void login() throws IOException {
 		System.out.print("사용자 ID 입력 : ");
 		String id = in.readLine();
@@ -154,5 +230,5 @@ public class HouseMain {
 			System.out.println(user);
 		}
 		System.out.println("-------------------------------------------------------------------------------------");
-	}
+	} 
 }
