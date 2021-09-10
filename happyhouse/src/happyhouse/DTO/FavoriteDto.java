9cc33f72e9484642427b277dee2a21c4eddb2986
이정훈 <person00333@gmail.com>
2021-09-10 08:31:50 +0000

    Upload New File

---
+++ happyhouse/src/happyhouse/DTO/FavoriteDto.java
@@ -0,0 +1,47 @@
+package happyhouse.DTO;
+
+	public class FavoriteDto {
+
+		private int user_no;
+		private int dongcode;
+		private String dong;
+		private String gu;
+
+		public int getUser_no() {
+				return user_no;
+			}
+
+		public void setUser_no(int user_no) {
+				this.user_no = user_no;
+			}
+
+		public int getDongcode() {
+				return dongcode;
+			}
+
+		public void setDongcode(int dongcode) {
+				this.dongcode = dongcode;
+			}
+
+		public String getDong() {
+				return dong;
+			}
+
+		public void setDong(String dong) {
+				this.dong = dong;
+			}
+
+		public String getGu() {
+				return gu;
+			}
+
+		public void setGu(String gu) {
+				this.gu = gu;
+			}
+
+		@Override
+			public String toString() {
+				return "FavoriteDtoImpl [user_no=" + user_no + ", dongcode=" + dongcode + ", dong=" + dong + ", gu=" + gu + "]";
+			}
+
+	}
